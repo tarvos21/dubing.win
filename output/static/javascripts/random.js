b020253d77d3fc76992ae09798004d0e990bf799
Du Bing <tarvos21@gmail.com>
2017-02-11 23:42:03 +0800

    update files with new gitignore

--- output/static/javascripts/random.js
+++
@@ -1,7 +0,0 @@
-var parent = $('.ui.container.grid > .ui.row');
-var index = Math.floor(Math.random() * 18);
-var part1 = '<iframe\ width="640"\ height="360"\ src="https://www.youtube.com/embed/videoseries?list=PLGVZCDnMOq0rjkF7p_F4qtaVJQnjK1oKT&index=';
-var part2 = '"\ frameborder="0"\ allowfullscreen></iframe>';
-var element = part1 + index + part2;
-var $ele = $(element);
-$ele.insertAfter(parent);
--- output/type/README/index.html
+++
@@ -1,2 +0,0 @@
-# type
-A FOSS clone of [typing.io](https://typing.io). You can check it out at [type.shadowfacts.net](https://type.shadowfacts.net).
